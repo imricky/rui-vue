6f5d0e9eb04ddba33d16afb217fcfb25a1a043c3
rqcao <rqcao@outlook.com>
2018-11-19 16:13:15 +0800

    添加按钮，验证按钮位置（可配置） 添加按钮图标（可配置）

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,24 @@
 <template>
-  <button class="g-button">按钮</button>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 <script>
-  export default {}
+  export default {
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(value) {
+          return value === 'left' || value === 'right'
+        }
+      }
+    }
+  }
 </script>
 <style lang="scss">
   .g-button {
@@ -12,6 +28,10 @@
     border-radius: var(--border-radius);
     border: 1px solid var(--border-color);
     background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
     &:hover {
       border-color: var(--border-color-hover);
     }
@@ -20,6 +40,23 @@
     }
     &:focus {
       outline: none;
+    }
+    > .content {
+      order: 2;
+    }
+    > .icon {
+      order: 1;
+      margin-right: .1em;
+    }
+    &.icon-right {
+      > .content {
+        order: 1;
+      }
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .1em;
+      }
     }
   }
 </style>
