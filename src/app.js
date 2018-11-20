51e2fea6522082c9ade7b79b33994d5cff8feaaf
rqcao <rqcao@outlook.com>
2018-11-20 21:41:04 +0800

    🔨 使用chai-spy对点击事件进行监听

--- src/app.js
+++ src/app.js
@@ -17,7 +17,8 @@ new Vue({
 })
 
 import chai from 'chai'
-
+import spies from 'chai-spies'
+chai.use(spies)
 const expect = chai.expect
 
 {
@@ -81,6 +82,21 @@ const expect = chai.expect
   expect(order).to.eq('2')
   vm.$el.remove()
   vm.$destroy()
+}
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  vm.$mount()
+  let spy = chai.spy(function(){})
+  vm.$on('click',spy)
+  let button = vm.$el
+  button.click()
+  expect(spy).to.have.been.called()
 }
 
 
