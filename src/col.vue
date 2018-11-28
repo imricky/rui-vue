366ef6d9dda628a300db081091a8ffa93bd84012
rqcao <rqcao@outlook.com>
2018-11-28 10:40:13 +0800

    重构下row和col

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,5 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-       :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
+  <div class="col" :class="colClass" :style="colStyle">
     <div style="border: 1px solid green; height: 100px;">
       <slot></slot>
     </div>
@@ -17,17 +16,26 @@
         type: [Number, String]
       },
     },
-    data () {
+    data() {
       return {
         gutter: 0
       }
     },
-    created () {
-      console.log('col created')
-    },
-    mounted () {
-      console.log('col mounted')
-    },
+    computed: {
+      colClass() {
+        let {span, offset} = this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle(){
+        return {
+          paddingLeft: this.gutter/2 +'px',
+          paddingRight: this.gutter/2 +'px',
+        }
+      }
+    }
   }
 </script>
 <style scoped lang="scss">
